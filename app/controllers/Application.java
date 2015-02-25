bf578dda26439484cdeb6613053d03d6ed57a0ab
Gordan Sajević <gsajevic@hotmail.com>
2015-02-25 18:47:54 +0100

    Update Application.java

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,40 +1,60 @@
-package models;
+package controllers;
 
-import javax.persistence.*;
+import models.User;
+import play.*;
+import play.data.Form;
+import play.mvc.*;
+import views.html.*;
 
-import play.data.validation.Constraints.MinLength;
-import play.data.validation.Constraints.Required;
-import play.db.ebean.Model;
-import play.db.ebean.Model.Finder;
-/**
- * 
- * @author Nermin Graca & Nedzad Hamzic & Neldin Dzekovic
- *
- */
-@Entity
-public class User extends Model {
-	
-	@Id
-	public int id;
-	
-	@Required
-	public String username;
-	
-	@Required
-	public String password;
-	
-	public User(String username, String password) {
-		this.username = username;
-		this.password = password;
-	}
-	
-	public static void create(String username, String password) {
-		new User(username, password).save();
-	}
+public class Application extends Controller {
 
-	static Finder<String, User> find = new Finder<String, User>(String.class, User.class);
+  static String message = "Welcome";
+  final static Form<User> userForm = form(User.class);
 	
-	public static User finder(String username) {
-		return find.where().eq("username", username).findUnique();
-	}
+  public static Result index() {
+    return ok(index.render(message, userForm));
+  }
+  
+  public static Result registration() {
+	Form<User> newUser = userForm.bindFromRequest();
+  	return ok(registration.render(newUser));
+  }
+  
+  public static Result login() {
+  	return ok(login.render());
+  }
+  
+  public static Result addUser() {
+  	String username = userForm.bindFromRequest().get().username;
+  	String password = userForm.bindFromRequest().get().password;
+  	User.create(username, password);
+  	return redirect("/");
+  	
+  }
+  
+  public static Result findUser() {
+  	String username = userForm.bindFromRequest().get().username;
+  	String password = userForm.bindFromRequest().get().password;
+  	User u = User.finder(username);
+  	if (u == null) {
+  		return redirect("/failed");
+  	} else {
+  		if (u.password.equals(password)) {
+  			return redirect("/success");
+  		} else {
+  			return redirect("/failed");
+  		}
+  		
+  	}
+  }
+  
+  public static Result success() {
+	String message = "Success!";
+  	return ok(success.render(message));
+  }
+  
+  public static Result failed() {
+	String message = "Failed!";
+  	return ok(failed.render(message));
+  }
 }
