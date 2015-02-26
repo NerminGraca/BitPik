933866ba30afb6b9ec83eb4fb95e90f0c9a86c6c
Gordan Sajević <gsajevic@hotmail.com>
2015-02-26 11:30:59 +0100

    updated html files

--- app/models/User.java
+++ app/models/User.java
@@ -19,17 +19,34 @@ public class User extends Model {
 	@Required
 	public String password;
 	
+	/**
+	 * Constructor
+	 * Creates a new User
+	 * @param username
+	 * @param password
+	 */
 	public User(String username, String password) {
 		this.username = username;
 		this.password = password;
 	}
 	
+	/**
+	 * Creates a new user and saves the User into database;
+	 * @param username
+	 * @param password
+	 */
 	public static void create(String username, String password) {
 		new User(username, password).save();
 	}
 
+	// Finder
 	static Finder<String, User> find = new Finder<String, User>(String.class, User.class);
 	
+	/**
+	 * Finds the User under the username(parameter) in the database;
+	 * @param username
+	 * @return
+	 */
 	public static User finder(String username) {
 		return find.where().eq("username", username).findUnique();
 	}
