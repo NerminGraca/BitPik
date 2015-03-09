781768d24136533be5e21ca67286be12709dcc41
NerminGraca <nera.graca@gmail.com>
2015-03-09 20:28:06 +0100

    Global limited to one load

--- app/Global.java
+++ app/Global.java
@@ -8,18 +8,22 @@ public class Global extends GlobalSettings {
 	
 	@Override
 	public void onStart(Application app) {
-		//Administrator entry
-		User ad = User.createAdmin("admin", "admin", "admin@admin.ba", true);	
-		UserController.insertAdmin(ad.username);
 		
-		//Main Categories entry
-		String categoryArray[] = {"Vozila", "Nekretnine", "Mobilni uređaji", 
-				"Kompjuteri", "Tehnika", "Nakit i satovi", "Moj dom", "Biznis i industrija",
-				"Životinje", "Odjeća i obuća", "Ostale kategorije"};
-		
-		for (int i = 0; i < categoryArray.length; i++) {
-			MainCategory.createMainCategory(categoryArray[i]);
+		if (User.finder("admin") == null) {
+			//Administrator entry
+			User ad = User.createAdmin("admin", "admin", "admin@admin.ba", true);	
+			UserController.insertAdmin(ad.username);
 		}
 		
+		if (MainCategory.findMainCategory(1) == null) {
+			//Main Categories entry
+			String categoryArray[] = {"Vozila", "Nekretnine", "Mobilni uređaji", 
+					"Kompjuteri", "Tehnika", "Nakit i satovi", "Moj dom", "Biznis i industrija",
+					"Životinje", "Odjeća i obuća", "Ostale kategorije"};
+			
+			for (int i = 0; i < categoryArray.length; i++) {
+				MainCategory.createMainCategory(categoryArray[i]);
+			}
+		}		
 	}
 }
