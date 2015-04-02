396c107fadac9703a4666035667bef7d68310cbb
NerminGraca <nera.graca@gmail.com>
2015-04-02 16:56:28 +0200

    buttons fixed

--- app/controllers/ProductController.java
+++ app/controllers/ProductController.java
@@ -252,23 +252,26 @@ public class ProductController extends Controller {
 			
 	}
 	
-//	public static void deleteOnePicture(int id, String imgPath){
-//		final String deletePath = "." + File.separator 
-//				+ "public" + File.separator;
-//		
-//		List<ImgPath> imgList= findProduct.byId(id).imgPathList;
-//		
-//		
-//		for (int i = 0; i< imgList.size() ; i++){
-//			String s = imgList.get(i).imgPath;
-//			
-//			if (!s.equals("images/no-img.jpg") && imgPath.equals(s)){
-//				File file = new File(deletePath + s);
-//				file.delete();
-//			}
-//						
-//		}
-//	}
+	public static Result deleteOnePicture(int id, String imgPath){
+		/*
+		final String deletePath = "." + File.separator 
+				+ "public" + File.separator;
+		
+		List<ImgPath> imgList= findProduct.byId(id).imgPathList;
+		
+		
+		for (int i = 0; i< imgList.size() ; i++){
+			String s = imgList.get(i).imgPath;
+			
+			if (!s.equals("images/no-img.jpg") && imgPath.equals(s)){
+				File file = new File(deletePath + s);
+				file.delete();
+			}
+						
+		}
+		*/
+		return TODO;
+	}
 	
 	/**
 	 * @param id is Product id
